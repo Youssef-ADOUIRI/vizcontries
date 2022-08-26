df0be2b67cda77efffa9c0f6d0e4de7cd2ada95d
Youssef-ADOUIRI <youssef.galaoui@gmail.com>
2022-08-26 20:35:46 +0100

    add about button

--- front/src/Nav.js
+++ front/src/Nav.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import './Nav.css'
+import React from "react";
+import "./Nav.css";
 
 function Nav() {
   return (
-    <div className='navbar'>
-        <a className='app-title'>VIS-COUNTRIES</a>
+    <div className="navbar">
+      <a className="app-title" href="#">VIS-COUNTRIES</a>
+      <ul className="navbar_ul">
+        <li className="about">
+          <a href="#" className="about_btn">about</a>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
