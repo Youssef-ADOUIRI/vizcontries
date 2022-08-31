804f93d6c3c4a0876c37b96901fb106eedbb40cc
Youssef-ADOUIRI <youssef.galaoui@gmail.com>
2022-08-31 22:41:14 +0100

    Db management

--- back/database/db_management.js
+++ back/database/db_management.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 
 DB_NAME = "watch_db"
 COL_NAME = "movies"
-PASSWORD = 'password'
+PASSWORD = 'UOHKqh4onoGhcRVH'
 
 Listening_server = `mongodb+srv://admin_user:${PASSWORD}@movie-picker.qadpv9w.mongodb.net/?retryWrites=true&w=majority`
 
