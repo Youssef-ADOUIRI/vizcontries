72c2accbb4b73e7f6b144fca0d4c209bd04007f0
Youssef-ADOUIRI <youssef.galaoui@gmail.com>
2022-09-01 14:58:56 +0100

    connecting to db

--- back/database/DB.js
+++
@@ -1,2 +0,0 @@
-const DB = "mongodb://localhost:27017/reactexpress";
-export default DB;
--- back/database/db_management.js
+++ back/database/db_management.js
@@ -1,11 +1,54 @@
-//inset in 
-import mongoose from 'mongoose'
+//inset in
+import * as dotenv from "dotenv";
+dotenv.config();
 
-DB_NAME = "watch_db"
-COL_NAME = "movies"
-PASSWORD = 'UOHKqh4onoGhcRVH'
+import mongoose from "mongoose";
 
-Listening_server = `mongodb+srv://admin_user:${PASSWORD}@movie-picker.qadpv9w.mongodb.net/?retryWrites=true&w=majority`
+export let myclient = mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    () => {
+      console.log("Database is connected");
+    },
+    (err) => {
+      console.log("Cannot connect to the database : " + err);
+    }
+  );
 
+export const Schema = {
+  Id: {
+    type: String,
+    required: true,
+  },
+  Name: String,
+  Available: {
+    type: Boolean,
+    required: true,
+  },
+  Population: Number,
+  GDP: Number,
+  GDP_Capita: Number,
+  CO2_emi: Number,
+  birth_rate: Number,
+  patent_app: Number,
+};
 
- 
+export const countries_schema = new mongoose.Schema(Schema);
+export const Countries = mongoose.model("countries", countries_schema);
+
+export const addtodb = (obj) => {
+  //insert in and save
+  if (obj == null) {
+    console.log("The object is undefined");
+    return null;
+  }
+  const coutry = new Countries(obj);
+  coutry
+    .save()
+    .then(() => console.log("One entry added"))
+    .catch((e) => console.error(e));
+  return coutry;
+};
